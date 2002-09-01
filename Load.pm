540e63f5b846a9d63911cb7882bdcd4211217e65
Peter Barabas <barabas@cpan.org>
2002-09-01 10:01:55 +0000

    Import of BARABAS/Sys-Load-0.2 from CPAN.
    gitpan-cpan-distribution: Sys-Load
    gitpan-cpan-version:      0.2
    gitpan-cpan-path:         BARABAS/Sys-Load-0.2.tar.gz
    gitpan-cpan-author:       BARABAS
    gitpan-cpan-maturity:     released

--- Load.pm
+++ Load.pm
@@ -1,3 +1,4 @@
+
 package Sys::Load;
 
 use 5.006;
@@ -9,31 +10,44 @@ require DynaLoader;
 
 our @ISA = qw(Exporter DynaLoader);
 
-our @EXPORT_OK = qw( getload );
+our @EXPORT_OK = qw( getload uptime );
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 bootstrap Sys::Load $VERSION;
 
 # Preloaded methods go here.
 
+use constant UPTIME => "/proc/uptime";
+
+sub uptime {
+  open(FILE, UPTIME) || return 0;
+  my $line = <FILE>;
+  my($uptime, $idle) = split /\s+/, $line;
+  close FILE;
+  return $uptime;
+}
+
 1;
+
 __END__
 
 =head1 NAME
 
-Sys::Load - Perl module for getting the current system load
+Sys::Load - Perl module for getting the current system load and uptime
 
 =head1 SYNOPSIS
 
-  use Sys::Load qw/getload/;
-  my @load = getload();
-  print "System load: ", $load[0], "\n";
+  use Sys::Load qw/getload uptime/;
+  print "System load: ", (getload())[0], "\n";
+  print "System uptime: ", int uptime(), "\n";
 
 =head1 DESCRIPTION
 
 getload() returns 3 elements: representing load averages over the last 1, 5 and
 15 minutes. On failure empty list is returned.
+
+uptime() returns the system uptime in seconds. Returns 0 on failure.
 
 =head2 EXPORT
 
@@ -45,7 +59,7 @@ Peter BARABAS, E<lt>z0d [@] artifact [.] huE<gt>
 
 =head1 SEE ALSO
 
-L<getloadavg(3)>
+L<getloadavg(3)>, L<uptime(1)>
 
 =cut
 
--- test.pl
+++ test.pl
@@ -3,6 +3,6 @@
 
 use Test;
 BEGIN { plan tests => 1 };
-use Sys::Load qw/getload/;
-ok(getload()); # If we made it this far, we're ok.
+use Sys::Load;
+ok(1); # If we made it this far, we're ok.
 
